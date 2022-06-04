7edd6430f7ebd56e65959f4eef74d051eb3e0ffc
Vladzolotonosha <46163619+VladZtn@users.noreply.github.com>
2022-06-04 14:27:00 +0300

    Add files via upload

---
+++ j5/index.js
@@ -0,0 +1,56 @@
+let count0 = Number(prompt('Введите количество буханок хлеба'));
+let count1 = Number(prompt('Введите количество бутылок с молоком'));
+let count2 = Number(prompt('Введите количество яблок'));
+
+var arr = [
+  {
+    name: 'Bread',
+    amt: count0,
+    price: 12
+  },
+  {
+    name: 'Milk',
+    amt: count1,
+    price: 24
+  },
+  {
+    name: 'Apple',
+    amt: count2,
+    price: 6
+  }
+]
+
+let cost0 = arr[0].amt * arr[0].price;
+let cost1 = arr[1].amt * arr[1].price;
+let cost2 = arr[2].amt * arr[2].price;
+
+var check = function(){
+console.log('Наименнование продукта: '+arr[0].name +', ' +' Количество:', arr[0].amt +', ' + 'Цена:', cost0 +' UAH')
+console.log('Наименнование продукта: '+arr[1].name +', ' +' Количество:', arr[1].amt +', ' + 'Цена:', cost1 +' UAH')
+console.log('Наименнование продукта: '+arr[2].name +', ' +' Количество:', arr[2].amt +', ' + 'Цена:', cost2 +' UAH')
+}
+
+
+var all = function(){
+    var all_cost = cost0 + cost1 + cost2;
+    console.log('Общая сумма покупок: ' +all_cost + ' UAH');
+}
+
+var expensive = function(){
+    var max = Math.max(cost0, cost1, cost2)
+        console.log('Самая дорогая покупка: ' +max+ ' UAH');
+}  
+
+var average = function(){
+    var medium0 = arr[0].price / cost0;
+    var medium1 = arr[1].price / cost1;
+    var medium2 = arr[2].price / cost2;
+    console.log('Средняя цена Хлеба - ' + Math.round(medium0) +' UAH , ' 
+    + 'Средняя цена Молока - '+ Math.round(medium1) +' UAH , '
+    + 'Средняя цена Яблок - ' + Math.round(medium2) +' UAH');
+}
+
+check()
+all()
+expensive()
+average()
