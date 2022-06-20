09ce2cb78ae40a7e55366d0522544016c0435536
Vladzolotonosha <46163619+VladZtn@users.noreply.github.com>
2022-06-20 18:48:32 +0300

    Add files via upload

---
+++ j6/js/main.js
@@ -0,0 +1,20 @@
+document.querySelector('button').onclick = score;
+
+function score(){
+    let firstnumb, secondnumb, sumb, result;
+
+    firstnumb = document.getElementById('first-numb').value;
+    secondnumb = document.getElementById('second-numb').value;
+    sumb = document.getElementById('select').value;
+
+    let arr = {
+        first: firstnumb,
+        second: secondnumb,
+        act: sumb
+    };
+
+    result = eval(arr.first + arr.act + arr.second);
+    document.getElementById("output").innerHTML = result;
+    
+}
+
