77ae357f8970cbdb110419283ac92bd320e65505
Vladzolotonosha <46163619+VladZtn@users.noreply.github.com>
2022-08-04 16:32:39 +0300

    Update script.js

--- j11/task2/script.js
+++ j11/task2/script.js
@@ -1,16 +1,24 @@
+let timerCounter = 0;
 let counter = 0;
-let timeElm = document.querySelector('.timer');
-let circleBlock = document.getElementById('btn-circle');
+const startText = document.querySelector('.startText');
+const hzBlock = document.querySelector('.clicker');
+const timeElm = document.querySelector('.timer');
+const circleBlock = document.getElementById('btn-circle');
 const out = document.getElementById('output');
 const elem = document.querySelector('.timer'); 
 const text = document.getElementById('text');
+const restart = document.getElementById('again');
+
 
 document.querySelector('.btn').onclick = function () {
     document.getElementById('btn1').hidden = true;
-    document.getElementById('text').style.display = "block";
+    document.querySelector('.btn').onclick = timer(30);
+    startText.style.display = "none";
+    text.style.display = "block";
+    hzBlock.style.display = "flex";
     circleBlock.style.display = "block";
     circleBlock.onclick = clicks;
-    document.getElementById('output').onclick = clicks;
+    out.onclick = clicks;
 }
 
 function clicks(){
@@ -21,17 +29,17 @@ function clicks(){
 function Clear(){
     circleBlock.style.display = 'none';
     text.style.display = 'none';
+    restart.style.display = 'block';
     out.disabled = true;
-    let outText = 'You`r result:';
+    let outText = 'Your result:';
     document.querySelector('.outText').innerText = outText;
 }
 
-function timer(x) {
-    if(x === -1) {
+function timer(timerCounter) {
+    if(timerCounter === -1) {
         Clear()
         return;
     }
-    timeElm.innerHTML = x;
-    return setTimeout(() => {timer(--x)}, 1000)
+    timeElm.innerHTML = timerCounter;
+    return setTimeout(() => {timer(--timerCounter)}, 1000)
 }
-timer(30)
