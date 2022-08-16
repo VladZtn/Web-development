09789d1759bef02ff75e47347c3ca155e2c29868
Vladzolotonosha <46163619+VladZtn@users.noreply.github.com>
2022-08-16 19:11:38 +0300

    Update script.js

--- j12/script.js
+++ j12/script.js
@@ -9,22 +9,6 @@ const startBlock = document.querySelector('.start__block');
 const playBlock = document.querySelector('.play__block');
 const chooseBlock = document.querySelector('.choose__block');
 const container = document.querySelectorAll(':is(.parent) img');
-//Dino`s
-const dinoOne = document.getElementById('dinoOne');
-const dinoTwo = document.getElementById('dinoTwo');
-const dinoThree = document.getElementById('dinoThree');
-const dinoFour = document.getElementById('dinoFour');
-const dinoFive = document.getElementById('dinoFive');
-const dinoSix = document.getElementById('dinoSix');
-const dinoSeven = document.getElementById('dinoSeven');
-const dinoEight = document.getElementById('dinoEight');
-const dinoNine = document.getElementById('dinoNine');
-const dinoTen = document.getElementById('dinoTen');
-const dinoEleven = document.getElementById('dinoEleven');
-const dinoTwelve = document.getElementById('dinoTwelve');
-const dinoThirtn = document.getElementById('dinoThirtn');
-const dinoFourtn = document.getElementById('dinoFourtn');
-const dinoFivetn = document.getElementById('dinoFivetn');
 
 
 
